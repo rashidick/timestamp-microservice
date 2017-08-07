c381d5adfa7787177f4ff19ba5b4a348720f4922
Rashid Ibragimov <xyxb23@gmail.com>
2017-08-07 10:09:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/views/index.html'));
+});
 
 app.get('/:timestamp', function(req, res) {
   var timestamp = req.params.timestamp;
